ee19997386470efc0f7a430cd6c63c35153fc697
Louis Magdaleno <buildthatapp@gmail.com>
2019-04-18 09:13:14 -0700

    Removed component state from app

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
 
 class App extends Component {
-  state = {
-    tech: "React"
-  };
+ // The state object has been removed
   render() {
     return <HelloWorld tech={this.state.tech} />;
   }
